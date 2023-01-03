3f66c4cd1eb26cffb54b6924b2ba9cd76cf3285b
Lander2039 <lander2039@gmail.com>
2023-01-03 13:29:27 +0300

    Added homework 10 Model View ViewModel + DI + SharedPreferences

--- app/src/main/java/com/example/androidhomework/HomeWork1/presentation/view/home/MainActivity.kt
+++ app/src/main/java/com/example/androidhomework/HomeWork1/presentation/view/home/MainActivity.kt
@@ -11,6 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
