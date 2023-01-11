ce42050fa419207cb1af4ee932c54b06ea2e20e6
Lander2039 <lander2039@gmail.com>
2023-01-11 18:58:06 +0300

    Added homework 10 Model View ViewModel + DI + SharedPreferences + Coroutines

--- app/src/main/java/com/example/androidhomework/HomeWork1/data/auth/AuthRepositoryImpl.kt
+++ app/src/main/java/com/example/androidhomework/HomeWork1/data/auth/AuthRepositoryImpl.kt
@@ -3,25 +3,35 @@ package com.example.androidhomework.HomeWork1.data.auth
 import com.example.kotlinlesson.data.sharedPreferences.SharedPreferencesHelper
 import com.example.kotlinlesson.domain.auth.AuthRepository
 import com.example.kotlinlesson.domain.model.UserModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(private val sharedPreferencesHelper: SharedPreferencesHelper) :
     AuthRepository {
 
     override suspend fun LoginUser(userName: String, userPassword: String) {
-        sharedPreferencesHelper.saveUserName(userName)
-        sharedPreferencesHelper.saveUserPassword(userPassword)
+        withContext(Dispatchers.IO) {
+            sharedPreferencesHelper.saveUserName(userName)
+            sharedPreferencesHelper.saveUserPassword(userPassword)
+        }
     }
 
     override suspend fun showUserCreds(): UserModel {
-        return sharedPreferencesHelper.getUserCreds()
+        return withContext(Dispatchers.IO) {
+            sharedPreferencesHelper.getUserCreds()
+        }
     }
 
     override suspend fun doesUserExist(): Boolean {
-        return sharedPreferencesHelper.checkUserExists()
+        return withContext(Dispatchers.IO) {
+            sharedPreferencesHelper.checkUserExists()
+        }
     }
 
     override suspend fun userLogout() {
-        sharedPreferencesHelper.removeUser()
+        return withContext(Dispatchers.IO) {
+            sharedPreferencesHelper.removeUser()
+        }
     }
 }
